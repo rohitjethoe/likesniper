6972b2c667df0a470d41755874790e3f73e69923
rohitjethoe <rohitchai@icloud.com>
2020-09-13 13:18:06 +0200

    removed ts.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,11 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
     ],
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
     "rules": {
     }
 };
