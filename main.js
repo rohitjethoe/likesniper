a505b87b6ade51df8cb0a53b86c93c4463daa4ea
rohitjethoe <rohitchai@icloud.com>
2020-09-14 22:54:00 +0200

    Fetch Username reaidng cli function.

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const { resolve } = require('path');
 const App = require('./server/app');
 
+let user = {
+    username: null,
+    password: null
+}
+
+const fetchUsername = () => {
+    return new Promise((resolve, reject) => {
+        readline.question('Enter your username: ', answer => {
+            user.username = answer;
+            resolve();
+        });
+    });
+}
+
+const fetchPassword = () => {
+    return new Promise((resolve, reject) => {
+        readline.question('Enter your password: \n', answer => {
+            user.password = answer;
+            resolve();
+        });
+    });
+}
+
+const runBot = () => {
+    return new Promise((resolve, reject) => {
+        readline.question('Run the instagram bot? (y/n) ', async answer => {
+            if (answer === "y") {
+                await App.openBrowser();
+                await App.setLogin(user.username, user.password);
+                await App.openHashtag('programming');
+                setInterval(() => {
+                    App.likePosts()
+                }, 10001);
+            } else {
+                console.log('Cancelling... :(');
+            }
+        });
+    })
+}
+
 (async() => {
-    await App.openBrowser();
-    await App.setLogin();
+    await fetchUsername();
+    await fetchPassword();
+    await runBot();
 })()
